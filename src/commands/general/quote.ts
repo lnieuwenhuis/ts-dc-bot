1056a39357f189acd21f12c9ceddb22bffa365bf
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2025-03-09 23:01:28 +0100

    Changed builder and added stopgap quote command

---
+++ src/commands/general/quote.ts
@@ -0,0 +1,41 @@
+import { SlashCommandBuilder, ModalBuilder, TextInputStyle } from "discord.js";
+import { buildModalTextInputs } from "../../helpers/buildModalTextInputs";
+
+export const data = new SlashCommandBuilder()
+    .setName("quote")
+    .setDescription("Creates a beautiful quote")
+
+export async function execute(interaction: any) {    
+    const quoteModal = new ModalBuilder({
+        customId: 'quote-modal',
+        title: 'Quote',
+    });
+
+    const actionRows = buildModalTextInputs([
+        { customId: 'quote', label: 'Quote', style: TextInputStyle.Paragraph, required: true },
+        { customId: 'author', label: 'Author', style: TextInputStyle.Short, required: true },
+        { customId: 'source', label: 'Person Quoted', style: TextInputStyle.Short, required: true },
+        { customId: 'year', label: 'Year', style: TextInputStyle.Short, required: true },
+    ]);
+
+    quoteModal.addComponents(...actionRows);
+    await interaction.showModal(quoteModal);
+
+    interaction.awaitModalSubmit({
+        time: 60000,
+        filter: (i: any) => i.user.id === interaction.user.id,
+    }).then(async (interaction: any) => {
+        const quote = interaction.fields.getTextInputValue('quote');
+        const author = interaction.fields.getTextInputValue('author');
+        const source = interaction.fields.getTextInputValue('source');
+        const year = interaction.fields.getTextInputValue('year');
+
+        const quoteEmbed = {
+            color: 0x0099FF,
+            title: `${quote}`,
+            description: `${source} (${year}) \n\nQuoted by ${author}`,
+        };
+
+        await interaction.reply({ embeds: [quoteEmbed] });
+    });
+}
--- src/helpers/buildModalTextInputs.ts
+++ src/helpers/buildModalTextInputs.ts
@@ -1,6 +1,6 @@
 import { ActionRowBuilder, TextInputBuilder, TextInputStyle } from "discord.js";
 
-export const buildModalTextInputs = (inputs: { customId: string, label: string, style: TextInputStyle }[]): ActionRowBuilder<TextInputBuilder>[] => {
+export const buildModalTextInputs = (inputs: { customId: string, label: string, style: TextInputStyle, required?: boolean }[]): ActionRowBuilder<TextInputBuilder>[] => {
     const actionRows: ActionRowBuilder<TextInputBuilder>[] = [];
 
     inputs.forEach(input => {
@@ -8,6 +8,10 @@ export const buildModalTextInputs = (inputs: { customId: string, label: string, 
             .setCustomId(input.customId)
             .setLabel(input.label)
             .setStyle(input.style);
+
+        if (input.required) {
+            textInput.setRequired(true);
+        }
 
         const actionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(textInput);
 
